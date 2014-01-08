760976966aea0b63d7c08250e94ce675ad24f725
Justin Kopepasah <justin@kopepasah.com>
2014-01-08 16:52:12 -0600

    Initial markup for variants.

--- lib/product-features/temp-metabox-markup.php
+++ lib/product-features/temp-metabox-markup.php
@@ -2,52 +2,62 @@
 	<div class="it-exchange-existing-variant it-exchange-existing-variant-543" data-variant-id="543">
 		<div class="variant-title variant-title-543" data-variant-id="543">
 			Leather Type
-			<span class="variant-title-values-preview varient-title-values-preview-543" data-variant-id="543">
+			<span class="variant-title-values-preview varient-title-values-preview-543" data-variant-id="543"></span>
+			<span class="variant-title-delete varient-title-delete-543 it-exchange-remove-item" data-variant-id="543">
+				&times;
 			</span>
 		</div>
 		<div class="variant-values variant-values-543" data-variant-id="543">
-			<span class="label">Values <?php it_exchange_admin_tooltip( 'tooltip goes here' ); ?></span>
-			<ul class="varient-values-list variant-values-list-543" data-variant-id="543">
-				<li data-variant-value-id="123" data-variant-value-parent="543" >
-					<span class="variant-value-reorder variant-value-reorder-123" data-variant-value-order="1" data-variant-value-id="123" data-variant-value-parent="543" >
-						<img src="http://f.cl.ly/items/1d0P320R1n011S301630/Screen%20Shot%202014-01-08%20at%2011.17.12%20AM.png" data-variant-value-id="123" data-variant-value-parent="543"/>
-					</span>
-					<input type="radio" class="variant-value-default-123" name="default-for-variant-123" data-variant-value-id="123" data-variant-value-parent="543" />
-					<span class="variant-value-name variant-value-name-123" data-variant-value-id="123" data-variant-value-parent="543" >Full Grain</span>
-					<span class="variant-value-image variant-value-image-123 variant-value-has-image" data-variant-value-id="123" data-variant-value-parent="543" >
-						<img src="http://f.cl.ly/items/0B2o3K073h3o1T0m2Z0u/Screen%20Shot%202014-01-08%20at%2010.55.09%20AM.png" data-variant-value-id="123" data-variant-value-parent="543"/>
-					</span>
-					<span class="variant-value-delete variant-value-delete-123" data-variant-value-id="123" data-variant-value-parent="543" >&times;</span>
-				</li>
-				<li data-variant-value-id="231" data-variant-value-parent="543" >
-					<span class="variant-value-reorder variant-value-reorder-231" data-variant-value-order="2" data-variant-value-id="231" data-variant-value-parent="543" >
-						<img src="http://f.cl.ly/items/1d0P320R1n011S301630/Screen%20Shot%202014-01-08%20at%2011.17.12%20AM.png" data-variant-value-id="123" data-variant-value-parent="543"/>
-					</span>
-					<input type="radio" class="variant-value-default-231" name="default-for-variant-231" data-variant-value-id="231" data-variant-value-parent="543" />
-					<span class="variant-value-name variant-value-name-231"data-variant-value-id="231" data-variant-value-parent="543" >Top Grain</span>
-					<span class="variant-value-image variant-value-image-231"data-variant-value-id="231" data-variant-value-parent="543" >
-						<img src="http://f.cl.ly/items/3h0d0T0u1p2Z1l3s2u1r/Screen%20Shot%202014-01-08%20at%2011.00.34%20AM.png" data-variant-value-id="231" data-variant-value-parent="543" />
-					</span>
-					<span class="variant-value-delete variant-value-image-231"data-variant-value-id="231" data-variant-value-parent="543" >&times;</span>
-				</li>
-				<li data-variant-value-id="562" data-variant-value-parent="543" >
-					<span class="variant-value-reorder variant-value-reorder-562" data-variant-value-order="3" data-variant-value-id="562" data-variant-value-parent="543" >
-						<img src="http://f.cl.ly/items/1d0P320R1n011S301630/Screen%20Shot%202014-01-08%20at%2011.17.12%20AM.png" data-variant-value-id="123" data-variant-value-parent="543"/>
-					</span>
-					<input type="radio" class="variant-value-default-562" name="default-for-variant-562" data-variant-value-id="562" data-variant-value-parent="543" />
-					<span class="variant-value-name variant-value-name-562"data-variant-value-id="562" data-variant-value-parent="543" >Faux Grain</span>
-					<span class="variant-value-image variant-value-image-562"data-variant-value-id="562" data-variant-value-parent="543" >
-						<img src="http://f.cl.ly/items/3h0d0T0u1p2Z1l3s2u1r/Screen%20Shot%202014-01-08%20at%2011.00.34%20AM.png" data-variant-value-id="562" data-variant-value-parent="543" />
-					</span>
-					<span class="variant-value-delete variant-value-image-562"data-variant-value-id="562" data-variant-value-parent="543" >&times;</span>
-				</li>
-			</ul>
-			<div class="add-variant-value add-variant-value-543" data-variant-value-parent="543">
-				<input type="button" value="Add Value" />
+			<div class="edit-variant">
+				<span class="label">Values <?php it_exchange_admin_tooltip( 'tooltip goes here' ); ?></span>
+				<ul class="varient-values-list variant-values-list-543" data-variant-id="543">
+					<li class="clearfix" data-variant-value-id="123" data-variant-value-parent="543" >
+						<div class="variant-value-reorder variant-value-reorder-123" data-variant-value-order="1" data-variant-value-id="123" data-variant-value-parent="543" ></div>
+						<div class="variant-value-info">
+							<input type="radio" class="variant-value-default-123" name="default-for-variant-123" data-variant-value-id="123" data-variant-value-parent="543" />
+							<span class="variant-value-name variant-value-name-123" data-variant-value-id="123" data-variant-value-parent="543" >Full Grain</span>
+							<span class="variant-value-image variant-value-image-123 variant-value-has-image" data-variant-value-id="123" data-variant-value-parent="543" >
+								<img src="http://f.cl.ly/items/0B2o3K073h3o1T0m2Z0u/Screen%20Shot%202014-01-08%20at%2010.55.09%20AM.png" data-variant-value-id="123" data-variant-value-parent="543"/>
+							</span>
+						</div>
+						<div class="variant-value-delete variant-value-delete-123" data-variant-value-id="123" data-variant-value-parent="543" >
+							<a href class="it-exchange-remove-item">&times;</a>
+						</div>
+					</li>
+					<li class="clearfix" data-variant-value-id="231" data-variant-value-parent="543" >
+						<div class="variant-value-reorder variant-value-reorder-123" data-variant-value-order="1" data-variant-value-id="123" data-variant-value-parent="543" ></div>
+						<div class="variant-value-info">
+							<input type="radio" class="variant-value-default-123" name="default-for-variant-123" data-variant-value-id="123" data-variant-value-parent="543" />
+							<span class="variant-value-name variant-value-name-123" data-variant-value-id="123" data-variant-value-parent="543" >Top Grain</span>
+							<span class="variant-value-image variant-value-image-123 variant-value-has-image" data-variant-value-id="123" data-variant-value-parent="543" >
+								<img src="http://f.cl.ly/items/3h0d0T0u1p2Z1l3s2u1r/Screen%20Shot%202014-01-08%20at%2011.00.34%20AM.png" data-variant-value-id="123" data-variant-value-parent="543"/>
+							</span>
+						</div>
+						<div class="variant-value-delete variant-value-delete-123" data-variant-value-id="123" data-variant-value-parent="543" >
+							<a href class="it-exchange-remove-item">&times;</a>
+						</div>
+					</li>
+					<li class="clearfix" data-variant-value-id="562" data-variant-value-parent="543" >
+						<div class="variant-value-reorder variant-value-reorder-123" data-variant-value-order="1" data-variant-value-id="123" data-variant-value-parent="543" ></div>
+						<div class="variant-value-info">
+							<input type="radio" class="variant-value-default-123" name="default-for-variant-123" data-variant-value-id="123" data-variant-value-parent="543" />
+							<span class="variant-value-name variant-value-name-123" data-variant-value-id="123" data-variant-value-parent="543" >Faux Grain</span>
+							<span class="variant-value-image variant-value-image-123 variant-value-has-image" data-variant-value-id="123" data-variant-value-parent="543" >
+								<img src="http://f.cl.ly/items/3h0d0T0u1p2Z1l3s2u1r/Screen%20Shot%202014-01-08%20at%2011.00.34%20AM.png" data-variant-value-id="123" data-variant-value-parent="543"/>
+							</span>
+						</div>
+						<div class="variant-value-delete variant-value-delete-123" data-variant-value-id="123" data-variant-value-parent="543" >
+							<a href class="it-exchange-remove-item">&times;</a>
+						</div>
+					</li>
+				</ul>
+				<div class="add-variant-value add-variant-value-543" data-variant-value-parent="543">
+					<input type="button" class="button" value="Add Value" />
+				</div>
 			</div>
 			<div class="save-variant save-variant-543" data-variant-id="543">
-				<input type="button" value="Cancel" />
-				<input type="button" value="Save" />
+				<input type="button" class="button" value="Cancel" />
+				<input type="button" class="button-primary" value="Save" />
 			</div>
 		</div>
 	</div>
@@ -59,7 +69,7 @@
 			<span class="variant-title-values-preview varient-title-values-preview-643" data-variant-id="643">
 				Extra Small, Small, Medium, Large, Extra Large
 			</span>
-			<span class="variant-title-delete varient-title-delete-643" data-variant-id="643">
+			<span class="variant-title-delete varient-title-delete-643 it-exchange-remove-item" data-variant-id="643">
 				&times;
 			</span>
 		</div>
@@ -72,7 +82,7 @@
 			<span class="variant-title-values-preview varient-title-values-preview-743" data-variant-id="743">
 				Red, Blue, Yellow, Green, Puke, Black, White, Clear
 			</span>
-			<span class="variant-title-delete varient-title-delete-743" data-variant-id="743">
+			<span class="variant-title-delete varient-title-delete-743 it-exchange-remove-item" data-variant-id="743">
 				&times;
 			</span>
 		</div>
