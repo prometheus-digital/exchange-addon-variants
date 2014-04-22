23ba59d0be0771fd51e822d4fa7a8bb86683c4d0
Ty Carlson <ty@ithemes.com>
2014-04-22 14:46:02 -0500

    initial variant front-end styling

--- lib/templates/content-product/elements/exchange-variants.php
+++ lib/templates/content-product/elements/exchange-variants.php
@@ -19,48 +19,52 @@
 	<div class="it-exchange-product-variants">
 		<?php do_action( 'it_exchange_content_product_begin_variants_element' ); ?>
 		<!-- Start HARDCODED DIVS for Ty -->
-		<div class="variant-options variant-select-options">
-			<h3 class="variant-title">Dropdown Variants</h3>
-			<select class="variant-options">
+		<div class="it-variant-options it-variant-select-options">
+			<div class="it-variant-title">Dropdown Variants</div>
+			<select class="it-variant-select">
 				<option>Select Option One</option>
 				<option selected>Select Option Two</option>
 				<option>Select Option Three</option>
 			</select>
 		</div>
 
-		<div class="variant-options variant-radio-options">
-			<h3 class="variant-title">Radio Variants</h3>
-			<ul class="variant-radios">
-				<li><label><input type="radio" class="variant-options" checked/> Radio Option One</label></li>
-				<li><label><input type="radio" class="variant-options" /> Radio Option Two</label></li>
-				<li><label><input type="radio" class="variant-options" /> Radio Option Three</label></li>
+		<div class="it-variant-options it-variant-radio-options">
+			<div class="vit-ariant-title">Radio Variants</div>
+			<ul class="it-variant-radios">
+				<li><label><input type="radio" class="it-variant-options" checked/> Radio Option One</label></li>
+				<li><label><input type="radio" class="it-variant-options" /> Radio Option Two</label></li>
+				<li><label><input type="radio" class="it-variant-options" /> Radio Option Three</label></li>
 			</ul>
 		</div>
 
-		<div class="variant-options variant-image-options">
-			<h3 class="variant-title">Image Variants</h3>
-			<div class-"variant-image">
-				<img src="http://placehold.it/75x75" />
-			</div>
-			<div class-"variant-image selected">
-				<img src="http://placehold.it/75x75" />
-			</div>
-			<div class-"variant-image">
-				<img src="http://placehold.it/75x75" />
-			</div>
+		<div class="it-variant-options it-variant-image-options">
+			<div class="it-variant-title">Image Variants</div>
+			<ul class="it-variant-image-list">
+				<li class="it-variant-image">
+					<img src="http://placehold.it/55x55" />
+				</li>
+				<li class="it-variant-image selected">
+					<img src="http://placehold.it/55x55" />
+				</li>
+				<li class="it-variant-image">
+					<img src="http://placehold.it/55x55" />
+				</li>
+			</ul>
 		</div>
 
-		<div class="variant-options variant-hex-options">
-			<h3 class="variant-title">Color Variants</h3>
-			<div class-"variant-color">
-				<div class="variant-color-inner" style="height:50px;width:50px;background:#F1FFDE"></div>
-			</div>
-			<div class-"variant-color selected">
-				<div class="variant-color-inner" style="height:50px;width:50px;background:#0082CA"></div>
-			</div>
-			<div class-"variant-color">
-				<div class="variant-color-inner" style="height:50px;width:50px;background:#334940"></div>
-			</div>
+		<div class="it-variant-options it-variant-hex-options">
+			<div class="it-variant-title">Color Variants</div>
+			<ul class="it-variant-hex-list">
+				<li class="it-variant-color">
+					<div class="it-variant-color-inner" style="height:55px;width:55px;background:#F1FFDE"></div>
+				</li>
+				<li class="it-variant-color selected">
+					<div class="it-variant-color-inner" style="height:55px;width:55px;background:#0082CA"></div>
+				</li>
+				<li class="it-variant-color">
+					<div class="it-variant-color-inner" style="height:55px;width:55px;background:#334940"></div>
+				</li>
+			</ul>
 		</div>
 		<!-- END HARDCODED DIVS for Ty -->
 		<?php do_action( 'it_exchange_content_product_end_variants_element' ); ?>
