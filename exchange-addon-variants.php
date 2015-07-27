798ed5cb5defca22bb806bdfb65b658cb49ad114
Lew Ayotte <lew@ithemes.com>
2015-07-27 15:26:07 -0500

    Tagging 1.2.0

--- exchange-addon-variants.php
+++ exchange-addon-variants.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange - Product Variants
- * Version: 1.1.2
+ * Version: 1.2.0
  * Description: Allows store owners to add variant options to iThemes Exchange product types.
  * Plugin URI: http://ithemes.com/purchase/product-variants/
  * Author: iThemes
